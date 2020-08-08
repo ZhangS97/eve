8a5fe702ca21c49f4dcb4b4f3ab23d56bcb12ef3
ZhangS97 <376195987@qq.com>
2020-08-08 11:36:03 +0800

    marketgroupCache doload换为单线程版本

--- src/main/java/com/demo/web/bundle/market/cache/MarketGroupCache.java
+++ src/main/java/com/demo/web/bundle/market/cache/MarketGroupCache.java
@@ -2,17 +2,14 @@ package com.demo.web.bundle.market.cache;
 
 import com.alibaba.fastjson.JSON;
 import com.demo.component.CacheableTree;
-import com.demo.utils.ListUtils;
-import com.demo.utils.SpringUtils;
 import com.demo.web.bundle.market.entity.MarketGroup;
 import com.demo.web.bundle.market.model.service.MarketGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,14 +17,13 @@ import java.util.Map;
 public class MarketGroupCache extends CacheableTree
 {
 
+    public static final String NAME = "";
+
     public static final String KEY_ID = "market_group:id:";
 
     public static final String KEY_CHILDREN_ID = "market_group:children_id:";
 
     public static final String KEY_PARENT_ID = "market_group:parent_id:";
-
-    //存放父id及其对应的下一层儿子id，双层树状结构
-    private static Map<String, List<String>> map = new Hashtable<>();
 
     @Autowired
     @Qualifier("marketGroupsService")
@@ -35,27 +31,13 @@ public class MarketGroupCache extends CacheableTree
 
     public void doLoad()
     {
-        List<List<MarketGroup>> allMarketGroups = ListUtils.groupList(service.findAll());
+        List<MarketGroup> marketGroupList = service.findAll();
 
-        MarketGroupCache marketGroupCacheProxy = SpringUtils.getBean(
-                MarketGroupCache.class);
+        //存放父id及其对应的下一层儿子id，双层树状结构
+        Map<String, List> map = new HashMap<>();
 
-        for (List<MarketGroup> marketGroupList : allMarketGroups)
-        {
-            marketGroupCacheProxy.doLoadTask(marketGroupList);
-        }
-
-        for (String str : map.keySet())
-        {
-            saveOne(KEY_PARENT_ID + str, JSON.toJSONString(map.get(str)));
-        }
-    }
-
-    @Async
-    void doLoadTask(List<MarketGroup> marketGroups)
-    {
         List<String> list;
-        for (MarketGroup marketGroup : marketGroups)
+        for (MarketGroup marketGroup : marketGroupList)
         {
             //根据父id 查看是否存在兄弟list
             list = map.get(marketGroup.getParentGroupId());
@@ -76,16 +58,17 @@ public class MarketGroupCache extends CacheableTree
             //存储当前对象
             saveOne(KEY_ID + marketGroup.getMarketGroupId(),
                     JSON.toJSONString(marketGroup));
-
-            //存储子级物品单位
-            if (null != marketGroup.getTypes() ||
-                    (!"".equals(marketGroup.getTypes())))
-            {
-                //非空则存储
-                saveOne(KEY_CHILDREN_ID + marketGroup.getMarketGroupId(),
-                        marketGroup.getTypes());
-            }
-
+            //存储下级对象
+            saveOne(KEY_CHILDREN_ID + marketGroup.getMarketGroupId(),
+                    marketGroup.getTypes());
         }
+
+        //根据父级id存储下属所有节点
+        for (String str : map.keySet())
+        {
+            saveOne(KEY_PARENT_ID + str, JSON.toJSONString(map.get(str)));
+        }
+
     }
+
 }
