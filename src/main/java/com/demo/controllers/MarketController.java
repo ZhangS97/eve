1aaa092244730fe236ecf17ead48faaeddbd076c
ZhangS <376195987@qq.com>
2020-10-27 23:45:19 +0800

    update

--- src/main/java/com/demo/controllers/MarketController.java
+++ src/main/java/com/demo/controllers/MarketController.java
@@ -1,6 +1,7 @@
 package com.demo.controllers;
 
 import com.demo.service.MarketService;
+import com.demo.web.bundle.market.cache.MarketGroupCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +15,28 @@ public class MarketController
     @Autowired
     private MarketService service;
 
+    @Autowired
+    private MarketGroupCache marketGroupCache;
+
     @RequestMapping(value = "/markets/orders", method = RequestMethod.GET)
     @ResponseBody
     public String getOrders(int regionId)
     {
 
+        return null;
+    }
+
+    /**
+     * 查取当前节点的下一级节点
+     *
+     * @param regionId
+     * @return
+     */
+    @RequestMapping(value = "/markets/getMarketOrderTree", method = RequestMethod.GET)
+    @ResponseBody
+    public String getMarketOrderTree(int regionId)
+    {
+        marketGroupCache.fetchOne();
         return null;
     }
 }
