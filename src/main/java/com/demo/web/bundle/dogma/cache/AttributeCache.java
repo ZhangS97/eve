c8c49f9b04db54744eaf2b11ab0d381c6104b007
ZhangS <376195987@qq.com>
2020-11-09 23:15:39 +0800

    update

--- src/main/java/com/demo/web/bundle/dogma/cache/AttributeCache.java
+++ src/main/java/com/demo/web/bundle/dogma/cache/AttributeCache.java
@@ -4,4 +4,10 @@ import com.demo.component.cache.Cacheable;
 
 public class AttributeCache extends Cacheable
 {
+    public static final String NAME = "";
+
+    public static final String KEY_ID = "";
+
+    public static final String KEY_PARENT_ID = "";
+
 }
